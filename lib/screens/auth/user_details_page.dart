ce6d73679366f06e1f0a1d5b38d74a56b2c5add8
seuunng <mnb2856@gmail.com>
2025-01-22 20:39:21 +0900

    별명 추천 기능 생성

--- lib/screens/auth/user_details_page.dart
+++ lib/screens/auth/user_details_page.dart
@@ -14,7 +14,25 @@ class _UserDetailsPageState extends State<UserDetailsPage> {
   int? _birthYear; // 기본값 설정
   bool _agreedToPrivacyPolicy = false; // 개인정보 제공 동의 체크박스 상태
   TextEditingController _nicknameController = TextEditingController();
-
+  @override
+  void initState() {
+    super.initState();
+    _nicknameController = TextEditingController();
+    _setRandomNickname(); // 랜덤 별명 설정
+  }
+  // 두그룹 합쳐서 별명 만들기
+  String _generateRandomNickname() {
+    final randomAdjective = (adjectives.toList()..shuffle()).first;
+    final randomNoun = (nouns.toList()..shuffle()).first;
+    return '$randomAdjective$randomNoun';
+  }
+  //랜덤으로 하나골라서 추천하기
+  void _setRandomNickname() {
+    final randomNickname = _generateRandomNickname();
+    setState(() {
+      _nicknameController.text = randomNickname;
+    });
+  }
   void _saveUserDetails() async {
     if (!_agreedToPrivacyPolicy &&
         !((_selectedGender == '선택하지 않음' || _selectedGender == null) &&
@@ -95,6 +113,7 @@ class _UserDetailsPageState extends State<UserDetailsPage> {
       },
     );
   }
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
@@ -235,3 +254,83 @@ class _UserDetailsPageState extends State<UserDetailsPage> {
     );
   }
 }
+
+final List<String> adjectives = [
+  "행복한 ",
+  "푸른 ",
+  "밝은 ",
+  "용감한 ",
+  "멋진 ",
+  "부드러운 ",
+  "깨끗한 ",
+  "귀여운 ",
+  "따뜻한 ",
+  "재미있는 ",
+  "상냥한 ",
+  "활기찬 ",
+  "빛나는 ",
+  "다정한 ",
+  "깜찍한 ",
+  "든든한 ",
+  "우아한 ",
+  "고요한 ",
+  "아름다운 ",
+  "목마른 ",
+  "졸린 ",
+  "신나는 ",
+  "궁금한 ",
+  "날고싶은 ",
+  "쉬고싶은 ",
+  "숨고싶은 ",
+  "부끄러운 ",
+  "똑똑한 ",
+  "느긋한 ",
+  "엉뚱한 ",
+  "쫄깃쫄깃한 ",
+  "느끼한 ",
+  "화끈한 ",
+  "반짝이는 ",
+];
+
+final List<String> nouns = [
+  "마카롱",
+  "바게트",
+  "햄버거",
+  "소다캔",
+  "주전자",
+  "바나나",
+  "당근",
+  "초코칩",
+  "치즈볼",
+  "구름빵",
+  "솜사탕",
+  "치타",
+  "다람쥐",
+  "오리너구리",
+  "젤리곰",
+  "피카츄",
+  "루돌프",
+  "까마귀",
+  "코뿔소",
+  "아이스크림",
+  "붕어빵",
+  "삐약이",
+  "알약",
+  "팝콘",
+  "만두왕",
+  "감자칩",
+  "마요네즈",
+  "호빵맨",
+  "콩나물",
+  "초코우유",
+  "라면왕",
+  "찜닭",
+  "꿀떡",
+  "비빔밥",
+  "고구마",
+  "떡볶이",
+  "버블티",
+  "감자튀김",
+  "쥬스박스",
+  "피자조각",
+];
