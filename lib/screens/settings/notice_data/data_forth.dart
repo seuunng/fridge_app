143fd61a5f06af6521caa51c0834f938898d7c59
seuunng <mnb2856@gmail.com>
2025-02-24 13:24:02 +0900

    로컬 변경사항 커밋

---
+++ lib/screens/settings/notice_data/data_forth.dart
@@ -0,0 +1,17 @@
+import 'package:food_for_later_new/models/notice.dart';
+
+
+final Notice thirdNotice = Notice(
+  title: "서비스 약관 제정",
+  date: DateTime(2025, 1, 23),
+  content: """
+**서비스 약관 제정 안내**
+
+**이따 뭐먹지** 서비스 약관이 새롭게 제정되었습니다.  
+서비스 이용과 관련된 자세한 약관 내용을 아래 링크에서 확인하실 수 있습니다.
+
+---
+
+[서비스 약관 전문 보기](https://food-for-later.web.app/terms-of-service.html)
+""",
+);
